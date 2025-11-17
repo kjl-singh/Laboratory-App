77ef00263cfa448621f1fd6dd478a8084f73c6c7
vboxuser <vboxuser@ubuntu22.04.myguest.virtualbox.org>
2025-11-17 14:43:31 +0530

    Some more changes

--- laboratory/public/js/lab_request.js
+++ laboratory/public/js/lab_request.js
@@ -1,53 +1,39 @@
-frappe.ui.form.on("Lab Request", {
-
-    tests_add(frm, cdt, cdn) {
-        update_lines_table(frm);
+frappe.ui.form.on("Lab Request Test", {
+    lab_test(frm, cdt, cdn) {
+        update_line(frm, cdt, cdn);
     },
-
-    tests_remove(frm) {
-        update_lines_table(frm);
-    },
-
-    tests(frm) {
-        update_lines_table(frm);
+    selected(frm, cdt, cdn) {
+        update_line(frm, cdt, cdn);
     }
 });
 
-function update_lines_table(frm) {
+function update_line(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
 
-    if (!frm.doc.tests) return;
+    let line = frm.doc.lines.find(l => l.test_sample == row.name);
 
-    frm.clear_table("lines");
+    if (row.selected && row.lab_test) {
+        frappe.db.get_doc("Lab Test", row.lab_test).then(testDoc => {
+            if (!testDoc) return;
 
-    const selected = frm.doc.tests.filter(row => row.selected == 1 && row.lab_test);
-
-    if (selected.length === 0) {
-        frm.refresh_field("lines");
-        return;
-    }
-
-    selected.forEach(row => {
-
-        frappe.call({
-            method: "frappe.client.get",
-            args: {
-                doctype: "Lab Test",
-                name: row.lab_test
-            },
-            callback: function (res) {
-
-                if (!res.message) return;
-
-                const test = res.message;
-
-                const child = frm.add_child("lines");
-                child.test = test.service || test.name;
-                child.turnaround_time = test.turnaround_time || "";
-                child.sale_price = test.price || 0;
-                child.special_instruction = test.special_instruction || "";
-
-                frm.refresh_field("lines");
+            if (!line) {
+                line = frm.add_child("lines");
+                line.test_sample = row.name; 
             }
+
+            line.test = testDoc.name; 
+            line.test_name = testDoc.service || testDoc.name;
+            line.turnaround_time = testDoc.turnaround_time || "";
+            line.sale_price = testDoc.price || 0;
+            line.special_instruction = testDoc.special_instruction || "";
+
+            frm.refresh_field("lines");
         });
-    });
+    } else {
+        if (line) {
+            const index = frm.doc.lines.indexOf(line);
+            if (index !== -1) frm.doc.lines.splice(index, 1);
+            frm.refresh_field("lines");
+        }
+    }
 }
